3c4c51c3f1ac855046311205545aac56adb8dc8d
Sarah Choung <sarahjchoung@gmail.com>
2020-08-30 23:34:46 +0000

    progress bar implemented

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,29 +1,41 @@
 import React, { useState } from "react";
 import EditIcon from "@material-ui/icons/Edit";
-import TextField from '@material-ui/core/TextField';
-import Button from '@material-ui/core/Button';
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
 
 export default function Header(props) {
-  const [inputOpen, setInputOpen] = useState(false)
-  const [project, setProject] = useState(props.project)
-  const inputOrText = inputOpen
-    ? <form noValidate autoComplete="off" className="edit-project-form">
+  const completedTasks = props.completed.taskIds.length;
+  const totalTasks = Object.keys(props.tasks).length;
+  const completedFraction = completedTasks / totalTasks;
+  const completedPercentage = Math.round(completedFraction * 100);
+
+  const [inputOpen, setInputOpen] = useState(false);
+  const [project, setProject] = useState(props.project);
+  const inputOrText = inputOpen ? (
+    <form noValidate autoComplete="off" className="edit-project-form">
       <div className="edit-project-input">
         <TextField
           id="standard-basic"
           defaultValue={project}
           onChange={(event) => {
-            setProject(event.target.value)
+            setProject(event.target.value);
           }}
         />
       </div>
-      <Button mx={2} variant="contained" onClick={() => {
-        props.editProject(project)
-        setInputOpen(false)
-      }}>Apply</Button>
+      <Button
+        mx={2}
+        variant="contained"
+        onClick={() => {
+          props.editProject(project);
+          setInputOpen(false);
+        }}
+      >
+        Apply
+      </Button>
     </form>
-  :
+  ) : (
     <h2>{project}</h2>
+  );
 
   return (
     <header>
@@ -32,6 +44,15 @@ export default function Header(props) {
         <div className="edit-project-name">
           <EditIcon onClick={() => setInputOpen(true)} />
         </div>
+      </div>
+      <div className="progress-info">
+        <div className="progress-bar-container">
+          <div
+            style={{ width: `${completedFraction * 285}px` }}
+            className="progress"
+          ></div>
+        </div>
+        <span className="progress-number">{`${completedPercentage}% Completed`}</span>
       </div>
     </header>
   );
