f379ab5c737703c3a696365a4aa102fe33d0a15a
Adam Botma <adam.botma@me.com>
2020-08-30 01:20:15 +0000

    modal alerting user before delete added and funtioning

--- src/Components/KanbanColumn.js
+++ src/Components/KanbanColumn.js
@@ -2,8 +2,24 @@ import React from "react";
 import CardModal from "./CardModal";
 import { Droppable, Draggable } from "react-beautiful-dnd";
 import DeleteOutlineIcon from "@material-ui/icons/DeleteOutline";
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
 
 export default function KanbanColumn(props) {
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   return (
     <Draggable draggableId={props.column.id} index={props.index}>
       {(provided) => (
@@ -14,7 +30,34 @@ export default function KanbanColumn(props) {
         >
           <div className="column-name-container" {...provided.dragHandleProps}>
             <h2>{props.column.title}</h2>
-            <DeleteOutlineIcon onClick={() => props.deleteColumn(props.column.id)} />
+            <div>
+              <DeleteOutlineIcon onClick={handleClickOpen} />
+              <Dialog
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="alert-dialog-title"
+                aria-describedby="alert-dialog-description"
+              >
+                <DialogTitle id="alert-dialog-title">{"Are You sure about this?"}</DialogTitle>
+                <DialogContent>
+                  <DialogContentText id="alert-dialog-description">
+                    Deleting an entire column will result in deleting all of the tasks within it as well.
+                  </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                  <Button onClick={handleClose} color="primary">
+                    CANCEL
+          </Button>
+                  <Button onClick={()=> {
+                    props.deleteColumn(props.column.id);
+                    handleClose();
+                    }
+                    } color="primary" autoFocus>
+                    DELETE
+          </Button>
+                </DialogActions>
+              </Dialog>
+            </div>
           </div>
           <Droppable droppableId={props.column.id} type="task">
             {(provided) => (
@@ -26,13 +69,13 @@ export default function KanbanColumn(props) {
                 {props.tasks.map((task, index) => (
 
                   <CardModal deleteTask={props.deleteTask}
-                             editTask={props.editTask}
-                             currentColumn={props.column.id}
-                             editContent={props.editContent}
-                             key={task.id}
-                             task={task}
-                             index={index}
-                   />
+                    editTask={props.editTask}
+                    currentColumn={props.column.id}
+                    editContent={props.editContent}
+                    key={task.id}
+                    task={task}
+                    index={index}
+                  />
 
                 ))}
                 {provided.placeholder}
